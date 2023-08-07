e81645cb9b8bd1a914c8725e95a0b06794e99a38
sunghuncho <chohoon901@gmail.com>
2023-08-07 09:51:44 +0900

    special(1)

--- src/route/Special.js
+++ src/route/Special.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Category from '../component/Category'
+import './style/Best.scss'
+import List from '../component/List'
 
 function Special() {
   return (
-    <div>Special</div>
+    <div className='container'>
+        <div className='best'>
+            <Category></Category>
+            <List title='특가'></List>
+        </div>
+    </div>
   )
 }
 
